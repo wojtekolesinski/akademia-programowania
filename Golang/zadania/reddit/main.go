c27c4d7c0690f1c692564a02a38e44de20ea146d
wojtekolesinski <wojtek.olesinski00@gmail.com>
2023-03-28 15:16:00 +0200

    zadanie 2 merge

--- Golang/zadania/reddit/main.go
+++ Golang/zadania/reddit/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"reddit/fetcher"
 	"sync"
 	"time"
-	"io"
 )
 
 func main() {
@@ -41,14 +41,14 @@ func main() {
 			f = &fetcher.HttpRedditFetcher{Url: fmt.Sprintf("http://reddit.com/r/%s.json", sub), Headers: headers}
 
 			fmt.Printf("Fetching %s\n", sub)
-			ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond * 5000)
+			ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*5000)
 			defer cancel()
 			err := f.FetchWithContext(ctx)
 			if err != nil {
 				panic(err)
 			}
 
-			w, err := os.Create(fmt.Sprintf("./data/%s.txt", sub))
+			w, err = os.Create(fmt.Sprintf("./data/%s.txt", sub))
 			if err != nil {
 				panic(err)
 			}
